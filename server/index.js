e0334c108d83db4e12babf18a11b2f70187159a9
brianmsj <brianmsj@gmail.com>
2017-03-27 14:22:46 -0700

    added run server functions, close server and added modules in index.js file

---
+++ server/index.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router();
+const jsonParser = bodyParser.json();
+const mongoose = require('mongoose');
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Serve the build client
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// Unhandled requests which aren't for the API should serve index.html so
+// client-side routing using browserHistory can function
+app.get(/^(?!\/api(\/|$))/, (req, res) => {
+    const index = path.resolve(__dirname, '../client/build', 'index.html');
+    res.sendFile(index);
+});
+
+let server;
+function runServer(port=3001) {
+    return new Promise((resolve, reject) => {
+         mongoose.connect(DATABASE_URL, err => {
+            if(err) {
+              return reject(err);
+        }
+            console.log('Db connected');
+            server = app.listen(port, () => {
+                resolve();
+            }).on('error', reject);
+        });
+    });
+}
+
+function closeServer() {
+    return new Promise((resolve, reject) => {
+        server.close(err => {
+            if (err) {
+                return reject(err);
+            }
+            resolve();
+        });
+    });
+}
+
+if (require.main === module) {
+    runServer();
+}
+
+module.exports = {
+    app, runServer, closeServer
+};
